c1cbf14cc7c6070de807c62c040da0bc372700aa
hasan2733 <155909255+hasan2733@users.noreply.github.com>
2024-05-04 14:55:49 +0600

    Update snake__game.c

--- snake__game.c
+++ snake__game.c
@@ -2,91 +2,104 @@
 #include <conio.h>
 #include <time.h>
 #include <stdlib.h>
-#include<windows.h>
-void drawGame(int sx, int sy, int tx, int ty) {
+#include <windows.h>
+
+void drawGame(int sx, int sy, int tx, int ty, int fx, int fy, int prevX[], int prevY[], int prevCount) {
     system("cls");
-    for (int y = 10; y >= -10; y--) {
-        for (int x = -10; x <= 10; x++) {
-            if (x == sx && y == sy) {
-                printf("*");
-            } else if (x == tx && y == ty) {
-                printf("$");
-
+    for (int y = 11; y >= -11; y--) { 
+        for (int x = -11; x <= 11; x++) { 
+            int isBorder = (y == 11 || y == -11 || x == -11 || x == 11); 
+            int isPlayer = (x == sx && y == sy);
+            int isTarget = (x == tx && y == ty);
+            int isFootprint = (x == fx && y == fy);
+            int isPrevious = 0;
+            for (int i = 0; i < prevCount; i++) {
+                if (x == prevX[i] && y == prevY[i]) {
+                    isPrevious = 1;
+                    break;
+                }
             }
-
-            else {
-                 printf(" ");
+            if (isBorder) {
+                printf("#");
+            } else if (isPlayer) {
+                printf("*");
+            } else if (isTarget) {
+                printf("$");
+            } else if (isFootprint) {
+                printf(".");
+            } else if (isPrevious) {
+                printf(".");
+            } else {
+                printf(" ");
             }
         }
         printf("\n");
     }
 }
-void gotoxy(int x,int y)
-{
-    static HANDLE h = NULL;
-    if((!h)) h=GetStdHandle(STD_OUTPUT_HANDLE);
-    COORD c={x+2,y+3};
-    SetConsoleCursorPosition(h,c);
-}
-void Play_LEvel(int sx,int sy,int tlx,int tly)
-{
+
+void Play_LEvel(int sx, int sy, int tlx, int tly) {
     char ch;
-    while (1)
-    {
+    int fx = sx, fy = sy;
+    int prevX[100], prevY[100]; 
+    int prevCount = 0; 
+    while (1) {
         ch = getch();
-        switch (ch)
-        {
-        case 'w':
-        case 'W':
-            sy = sy + 1;
-            break;
-        case 's':
-        case 'S':
-            sy = sy - 1;
-            break;
-        case 'a':
-        case 'A':
-            sx = sx - 1;
-            break;
-        case 'd':
-        case 'D':
-            sx = sx + 1;
-            break;
+        prevX[prevCount] = sx; 
+        prevY[prevCount] = sy;
+        prevCount++;
+        if (prevCount >= 100) prevCount = 0;
+        if(ch == 'm' || ch == 'M') {
+            SetCursorPos(100, 100); 
+            continue;
         }
-         drawGame(sx, sy, tlx, tly);
+        switch (ch) {
+            case 'w':
+            case 'W':
+                sy = sy + 1;
+                break;
+            case 's':
+            case 'S':
+                sy = sy - 1;
+                break;
+            case 'a':
+            case 'A':
+                sx = sx - 1;
+                break;
+            case 'd':
+            case 'D':
+                sx = sx + 1;
+                break;
+        }
+        fx = sx;
+        fy = sy;
+        drawGame(sx, sy, tlx, tly, fx, fy, prevX, prevY, prevCount);
         if (sx == tlx && sy == tly) {
             printf("\nGAME OVER\n");
             sx = tlx;
             sy = tly;
             break;
         }
-        //gotoxy(4,4);
-        printf("%d%d\n",sx,sy);
-        if (sx == tlx && sy == tly)
-        {
+        printf("%d%d\n", sx, sy);
+        if (sx == tlx && sy == tly) {
             printf("\nGAME OVER\n");
-            sx=tlx;
-            sy=tly;
+            sx = tlx;
+            sy = tly;
             break;
         }
     }
-
 }
 
-int main()
-{
+int main() {
     char ch;
     clock_t start, end;
-    double cpu_time_used=0.0;
-    int level=0;
+    double cpu_time_used = 0.0;
+    int level = 0;
 
-    int sx=0,sy=0;
+    int sx = 0, sy = 0;
     int tx, ty;
 
-    while(1)
-    {
+    while (1) {
         srand(time(NULL));
-
 
         tx = rand() % 20 - 10;
         ty = rand() % 20 - 10;
@@ -96,22 +109,18 @@ int main()
         printf("Find the target ($) to win!\n\n");
 
         start = clock();
-        Play_LEvel(sx,sy,tx,ty);
+        Play_LEvel(sx, sy, tx, ty);
         end = clock();
         level++;
         cpu_time_used += ((double)(end - start)) / CLOCKS_PER_SEC;
         printf("if you want to continue ? press y for continue and n for discontinue:\n");
-        ch=getche();
-        if(ch=='n'||ch=='N')
-        {
+        ch = getche();
+        if (ch == 'n' || ch == 'N') {
             break;
         }
     }
-    printf("\n Time taken: %.3f seconds and Level=%d\n", cpu_time_used,level);
+    printf("\n Time taken: %.3f seconds and Level=%d\n", cpu_time_used, level);
     getch();
     getch();
     return 0;
 }
-
-
-
